549748275236873e3dbea5b23ca7433b6613eae2
Lucas Bordini <lucas.bordini@hotmail.com>
2019-09-24 15:58:20 -0300

    first commit

---
+++ app/src/main/java/com/veronezzi/guilherme/frasedodia/MainActivity.java
@@ -0,0 +1,57 @@
+package com.veronezzi.guilherme.frasedodia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textoNovaFrase;
+    private Button botaoNovaFrase;
+
+    private String[] frases = {
+            "teste",
+            "teste2",
+            "teste3",
+            "teste4",
+            "teste5",
+            "teste6",
+            "teste7",
+            "teste8",
+            "teste9",
+            "teste10",
+
+
+
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textoNovaFrase = findViewById(R.id.textoNovaFraseId);
+        botaoNovaFrase = findViewById(R.id.botaoNovaFraseId);
+
+        botaoNovaFrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(frases.length);
+
+                textoNovaFrase.setText( frases[numeroAleatorio] );
+
+            }
+        });
+
+
+    }
+}
